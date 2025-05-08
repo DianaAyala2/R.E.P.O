588bee5d94c6f06714a15d29ea565d8a603bf01d
DianaAyala2 <00021325@uca.edu.sv>
2025-05-07 22:02:38 -0600

    Ejercicio contraseña y usuario

---
+++ Condicionales/User.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main (){
+    char user, carnet, password, condicional;
+    cout << "Hi :), please enter your user and password to log in: "<<endl;
+    cout << "User: "<<endl;
+    cin >> user;
+    cout << "Password: "<<endl;
+    cin >>password;
+
+    if (user==carnet && password==condicional)
+    {
+        cout << "The user and password are correct :), access granted. ";
+    } else {
+        cout << "The user and password are incorrect :(. ";
+    }
+    return 0;
+}
