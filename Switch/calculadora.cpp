c9a39f1983ce86df4fa3c76ba885505d826fc4a3
DianaAyala2 <00021325@uca.edu.sv>
2025-05-12 23:03:14 -0600

    update

---
+++ Switch/calculadora.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main (){
+    int num1, num2, result;
+    char option;
+    cout << "Hi:), enter two numbers different from 0: "<<endl;
+    cout << "Num1: ";
+    cin>>num1;
+    cout<< "Num2: ";
+    cin>>num2;
+    cout << "select an arithmetic operation: "<<endl;
+    cout<< "a. Addition"<<endl;
+    cout << "b. Subtraction"<<endl;
+    cout << "c. Multiplication"<<endl;
+    cout << "d. Division"<<endl;
+    cout<< "select an option: ";
+    cin>>option;
+
+    if (option=='a') {
+        cout << "The result is: ";
+    } else if (option=='b'){
+        cout <<"The result is: ";
+    } else if (option=='c'){
+        cout << "The result is: ";
+    } else if (option=='d'){
+        cout << "The result is: ";
+    } else {
+        cout<< "select a valid option"<<endl;
+    }
+
+    switch (option) {
+        case 'a': 
+        result=num1+num2;
+        cout << result <<endl;
+        break;
+        case 'b':
+        result=num1-num2;
+        cout << result <<endl;
+        break;
+        case 'c':
+        result = num1 * num2;
+        cout << result <<endl;
+        break;
+        case 'd':
+        result=num1/num2;
+        cout << result <<endl;
+        break;
+    }
+return 0;
+}
