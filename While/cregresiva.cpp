634f3a70ccfa7da719f341b4ebf6221ad1e546f0
DianaAyala2 <00021325@uca.edu.sv>
2025-06-06 09:54:00 -0600

    ejercicio 7

---
+++ While/cregresiva.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    cout << "Holaaa, por favor ingresa un numero: ";
+    cin >> n;
+
+    while (n>=0){
+        cout << n << " " << endl;
+        n--;
+    }
+    return 0;
+}
